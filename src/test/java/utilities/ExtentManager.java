cc56d157e9459b4d60757ab44b4d448d10d2eeb6
patelharish <patelharish304@gmail.com>
2025-07-16 14:56:44 +0530

    updated extentreport

--- src/test/java/utilities/ExtentManager.java
+++ src/test/java/utilities/ExtentManager.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
@@ -10,7 +13,9 @@ public class ExtentManager {
 
     public static ExtentReports getExtentReports() {
         if (extent == null) {
-            ExtentSparkReporter sparkReporter = new ExtentSparkReporter("reports/extent-report.html");
+        	String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String reportPath = System.getProperty("user.dir") + "/reports/ExtentReport_" + timestamp + ".html";
+            ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
             extent = new ExtentReports();
             
             //make attractive our extent report page
